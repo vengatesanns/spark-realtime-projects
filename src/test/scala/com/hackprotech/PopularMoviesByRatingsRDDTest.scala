bdc1d9ea40a4fdd442e99243d2574100bad28858
vengatesanns <vengat19amd@gmail.com>
2021-05-02 23:29:36 +0530

    Test Cases are updated

---
+++ src/test/scala/com/hackprotech/PopularMoviesByRatingsRDDTest.scala
@@ -0,0 +1,33 @@
+package com.hackprotech
+
+import com.hackprotech.PopularMoviesByRatingsRDD.{findPopularRatingMovies, loadMoviesMasterDetails}
+import org.apache.spark.SparkContext
+import org.scalatest.BeforeAndAfterAll
+import org.scalatest.funsuite.AnyFunSuite
+
+/**
+ * @author Vengatesan Nagarajan
+ */
+class PopularMoviesByRatingsRDDTest extends AnyFunSuite with BeforeAndAfterAll {
+
+  @transient var sparkContext: SparkContext = _
+
+  override protected def beforeAll(): Unit = {
+    sparkContext = new SparkContext("local[2]", "PopularMoviesByRatingsRDDTest")
+  }
+
+  override protected def afterAll(): Unit = {
+    sparkContext.stop()
+  }
+
+  test("FindPopularRatingMovies") {
+    val popularMoviesRDD = findPopularRatingMovies(sparkContext, "/home/vengat/Projects/DataSets/ml-10M100K/ratings.dat")
+    assert(popularMoviesRDD.filter(_._2 == 4.5).count() == 0, "popularMoviesRDD should have only popular movies which has highest ratings")
+  }
+
+  test("LoadMoivesMasterDetails") {
+    val moviesMasterDetails = loadMoviesMasterDetails(sparkContext, "/home/vengat/Projects/DataSets/ml-10M100K/movies.dat")
+    assert(moviesMasterDetails.count() != 0, "MoviesMasterDetails Should not be Zero")
+  }
+
+}
